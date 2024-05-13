1d896b440bd88228ef25ca41e45beea4cc9a7648
Moh Agus Setiawan <mohagussetiaone@gmail.com>
2024-05-13 17:00:56 +0700

    update: swiper

--- src/components/partials/widget/CardSlider.jsx
+++ src/components/partials/widget/CardSlider.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCards } from "swiper";
+import { EffectCreative } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/effect-cards";
 
@@ -21,29 +21,20 @@ const cardLists = [
     cardNo: "****  ****  **** 3945",
   },
 ];
+
 const CardSlider = () => {
   return (
     <div>
-      <Swiper effect={"cards"} grabCursor={true} modules={[EffectCards]}>
+      <Swiper effect={"cards"} grabCursor={true} modules={[EffectCreative]}>
         {cardLists.map((item, i) => (
           <SwiperSlide key={i}>
-            <div
-              className={`${item.bg} h-[200px] bg-gradient-to-r relative rounded-md z-[1] p-4 text-white`}
-            >
+            <div className={`${item.bg} h-[200px] bg-gradient-to-r relative rounded-md z-[1] p-4 text-white`}>
               <div className="overlay absolute left-0 top-0 h-full w-full -z-[1]">
-                <img
-                  src={visaCardImage}
-                  alt=""
-                  className="h-full w-full object-cover"
-                />
+                <img src={visaCardImage} alt="" className="h-full w-full object-cover" />
               </div>
               <img src={visaCardImage2} alt="" />
-              <div className="mt-[18px] font-semibold text-lg mb-[17px]">
-                {item.cardNo}
-              </div>
-              <div className="text-xs text-opacity-75 mb-[2px]">
-                Card balance
-              </div>
+              <div className="mt-[18px] font-semibold text-lg mb-[17px]">{item.cardNo}</div>
+              <div className="text-xs text-opacity-75 mb-[2px]">Card balance</div>
               <div className="text-2xl font-semibold">$10,975</div>
             </div>
           </SwiperSlide>
